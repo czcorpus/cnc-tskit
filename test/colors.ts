357b9c7df2275b9b4713d1ad3128c62ed6a54377
Tomas Machalek <tomas@machalek.xyz>
2020-02-25 11:54:57 +0100

    Define color ops as data last so we can use pipe()

--- test/colors.ts
+++ test/colors.ts
@@ -1,6 +1,7 @@
 
 import { Color } from '../src/colors';
 import { assert } from 'chai';
+import { pipe } from '../src/index';
 
 describe('Color#color2str', function () {
 
@@ -13,21 +14,35 @@ describe('Color#color2str', function () {
 describe('Color#luminosity', function () {
 
   it('converts 128, 128, 128 to half luminosity', function () {
-    assert.deepEqual(Color.luminosity([128, 128, 128, 1], 0.5), [64, 64, 64, 1]);
+    assert.deepEqual(Color.luminosity(0.5, [128, 128, 128, 1]), [64, 64, 64, 1]);
   });
 
   it('converts 128, 128, 128 to + 1/2 luminosity', function () {
-    assert.deepEqual(Color.luminosity([128, 128, 128, 1], 1.5), [192, 192, 192, 1]);
+    assert.deepEqual(Color.luminosity(1.5, [128, 128, 128, 1]), [192, 192, 192, 1]);
   });
 
   it('does not overflow to negative values', function () {
-    assert.deepEqual(Color.luminosity([128, 128, 128, 1], 0), [0, 0, 0, 1]);
+    assert.deepEqual(Color.luminosity(0, [128, 128, 128, 1]), [0, 0, 0, 1]);
   });
 
   it('does not accept negative values', function () {
     assert.throws(function () {
-      Color.luminosity([10, 10, 10, 1], -3);
+      Color.luminosity(-3, [10, 10, 10, 1]);
     });
+  });
+
+});
+
+describe('Color pipeability', function () {
+
+  it('allows piping color operations together', function () {
+    const x = pipe(
+      '#58D68D',
+      Color.importColor(0.7),
+      Color.luminosity(1.5),
+      Color.color2str()
+    );
+    assert.equal(x, 'rgba(132, 255, 212, 0.7)');
   });
 
 });
