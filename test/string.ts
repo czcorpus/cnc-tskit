fe19ca2b9b1f6de8aa079fa86491d37263e496d0
Tomas Machalek <tomas@machalek.xyz>
2020-03-06 14:58:12 +0100

    Fix unit tests

--- test/string.ts
+++ test/string.ts
@@ -25,41 +25,53 @@ import { assert } from 'chai';
 describe('Strings#shortenText', function () {
 
     it('handles zero input', function () {
-        const short = Strings.shortenText('   ', 2);
+        const short = Strings.shortenText('   ', 2, '');
         assert.equal(short, '');
     });
 
     it('handles short text', function () {
-        const short = Strings.shortenText(' hi people  ', 100);
+        const short = Strings.shortenText(' hi people  ', 100, '');
         assert.equal(short, 'hi people');
     });
 
     it('can take exact size if input matches', function () {
         //            01234567890123456789012345
         const text = 'lorem ipsum dolor sit amet';
-        const short = Strings.shortenText(text, 17);
+        const short = Strings.shortenText(text, 17, '');
         assert.equal(short, 'lorem ipsum dolor');
-        const short2 = Strings.shortenText(text, 18);
+        const short2 = Strings.shortenText(text, 18, '');
         assert.equal(short2, 'lorem ipsum dolor');
     });
 
     it('does not force an additional word to prevent overflow', function () {
         //            01234567890123456789012345
         const text = 'lorem ipsum dolor sit amet';
-        const short = Strings.shortenText(text, 13);
+        const short = Strings.shortenText(text, 13, '');
         assert.equal(short, 'lorem ipsum');
     });
 
     it('cuts too long word', function () {
         const text = 'loremipsumdolorsitamet';
-        const short = Strings.shortenText(text, 3);
+        const short = Strings.shortenText(text, 3, '');
         assert.equal(short, 'lor');
     });
 
     it('does not care about whitespace', function () {
         //            01234567890123456789012345
         const text = 'lorem              ipsum         dolor        sit     amet';
-        const short = Strings.shortenText(text, 13);
+        const short = Strings.shortenText(text, 13, '');
         assert.equal(short, 'lorem ipsum');
+    });
+
+    it('default suffix works', function () {
+        const text = 'lorem ipsum';
+        const short = Strings.shortenText(text, 6);
+        assert.equal(short, 'lorem\u2026');
+    });
+
+    it('custom suffix words', function () {
+        const text = 'lorem ipsum';
+        const short = Strings.shortenText(text, 6, ' etc.');
+        assert.equal(short, 'lorem etc.');
     });
 });
