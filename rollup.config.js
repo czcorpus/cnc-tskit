d025996ee7edaadad15b255101a1a8154617b603
Tomas Machalek <tomas@machalek.xyz>
2021-10-18 14:40:33 +0200

    Upgrade deps
    remove old TS plugin, replace with esbuild

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,27 @@
-import typescript from 'rollup-plugin-typescript2';
-import { terser } from "rollup-plugin-terser";
+/*
+ * Copyright 2018 Tomas Machalek <tomas.machalek@gmail.com>
+ * Copyright 2018 Institute of the Czech National Corpus,
+ *                Faculty of Arts, Charles University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import esbuild from 'rollup-plugin-esbuild';
+import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 import path from 'path';
+
+const external = ['chai', ...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})];
 
 export default [
 	{
@@ -11,19 +31,9 @@ export default [
 			file: pkg.browser,
 			format: 'umd'
         },
-        external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+        external,
 		plugins: [
-			typescript({
-                typescript: require('typescript'),
-                tsconfig: './tsconfig.json',
-                tsconfigOverride:  {
-                    compilerOptions: {
-                        declaration: false,
-                        lib: ['es5', 'dom', 'es2015.iterable'],
-                        target: 'es5',
-                    }
-                }
-            }),
+			esbuild(),
             terser()
 		]
     },
@@ -35,12 +45,9 @@ export default [
 			{ dir: path.dirname(pkg.main), format: 'cjs' },
 			{ dir: path.dirname(pkg.module), format: 'es' }
 		],
-        external: ['chai', ...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+        external,
 		plugins: [
-			typescript({
-                typescript: require('typescript'),
-                tsconfig: "./tsconfig.json",
-            })
+			esbuild()
 		]
     }
 ];
